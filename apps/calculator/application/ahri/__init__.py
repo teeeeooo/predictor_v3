b8c876e60b841e71ba2d1124e2652e4dfb90c3df
teeeeooo <279387763+teeeeooo@users.noreply.github.com>
2026-07-12 19:46:52 +0900

    feat(ahri): export product-aware application contracts

--- apps/calculator/application/ahri/__init__.py
+++ apps/calculator/application/ahri/__init__.py
@@ -10,20 +10,31 @@ from apps.calculator.application.ahri.hspf2_adapter import (
     AhriHspf2Options,
     AhriHspf2Summary,
 )
+from apps.calculator.application.ahri.hspf2_multicapacity_adapter import (
+    AHRI_HSPF2_DUAL_POINT_ORDER,
+    AHRI_HSPF2_TRIPLE_POINT_ORDER,
+)
 from apps.calculator.application.ahri.seer2_adapter import (
+    AHRI_SEER2_DUAL_POINT_ORDER,
     AHRI_SEER2_POINT_ORDER,
+    AHRI_SEER2_PRODUCT_POINT_ORDER,
     AHRI_SEER2_TEMPERATURES_C,
     AhriSeer2Adapter,
     AhriSeer2InputError,
+    AhriSeer2Options,
     AhriSeer2Summary,
 )
 
 __all__ = [
+    "AHRI_HSPF2_DUAL_POINT_ORDER",
     "AHRI_HSPF2_HIDDEN_DEFAULTS",
     "AHRI_HSPF2_OPTIONAL_POINTS",
     "AHRI_HSPF2_POINT_ORDER",
     "AHRI_HSPF2_TEMPERATURES_C",
+    "AHRI_HSPF2_TRIPLE_POINT_ORDER",
+    "AHRI_SEER2_DUAL_POINT_ORDER",
     "AHRI_SEER2_POINT_ORDER",
+    "AHRI_SEER2_PRODUCT_POINT_ORDER",
     "AHRI_SEER2_TEMPERATURES_C",
     "AhriHspf2Adapter",
     "AhriHspf2InputError",
@@ -31,5 +42,6 @@ __all__ = [
     "AhriHspf2Summary",
     "AhriSeer2Adapter",
     "AhriSeer2InputError",
+    "AhriSeer2Options",
     "AhriSeer2Summary",
 ]
